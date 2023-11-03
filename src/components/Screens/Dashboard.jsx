526c34e921becb0f1f362546f3f6c8e83e919e71
lelouch248 <karthikgkalai@gmail.com>
2023-11-04 03:51:45 +0530

    displaying excel file done

---
+++ src/components/Screens/Dashboard.jsx
@@ -0,0 +1,44 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
+import TableHead from "@mui/material/TableHead";
+import PropTypes from "prop-types";
+import "./../styling/Dashboard.css";
+
+const Dashboard = ({ excelData }) => {
+	return (
+		<div className="flex items-center justify-center h-screen Dashboard">
+			<div className="w-full max-w-3xl p-6 bg-gray-100 rounded-lg">
+				<TableContainer style={{ height: "70vh", overflowY: "auto" }}>
+					<Table>
+						<TableHead>
+							<TableRow>
+								{excelData.length > 0 &&
+									excelData[0].map((header, i) => (
+										<TableCell key={i}>{header}</TableCell>
+									))}
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{excelData.map((row, i) => (
+								<TableRow key={i}>
+									{row.map((cell, i) => (
+										<TableCell key={i}>{cell}</TableCell>
+									))}
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</TableContainer>
+			</div>
+		</div>
+	);
+};
+
+Dashboard.propTypes = {
+	excelData: PropTypes.array.isRequired,
+};
+
+export default Dashboard;
