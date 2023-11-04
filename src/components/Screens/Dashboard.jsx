d21a25fe32c3a59f680e6774dc840f118f50a6c2
lelouch248 <karthikgkalai@gmail.com>
2023-11-04 22:12:47 +0530

    added local storage function

--- src/components/Screens/Dashboard.jsx
+++ src/components/Screens/Dashboard.jsx
@@ -8,37 +8,47 @@ import PropTypes from "prop-types";
 import "./../styling/Dashboard.css";
 
 const Dashboard = ({ excelData }) => {
-	return (
-		<div className="flex items-center justify-center h-screen Dashboard">
-			<div className="w-full max-w-3xl p-6 bg-gray-100 rounded-lg">
-				<TableContainer style={{ height: "70vh", overflowY: "auto" }}>
-					<Table>
-						<TableHead>
-							<TableRow>
-								{excelData.length > 0 &&
-									excelData[0].map((header, i) => (
-										<TableCell key={i}>{header}</TableCell>
-									))}
-							</TableRow>
-						</TableHead>
-						<TableBody>
-							{excelData.map((row, i) => (
-								<TableRow key={i}>
-									{row.map((cell, i) => (
-										<TableCell key={i}>{cell}</TableCell>
-									))}
-								</TableRow>
-							))}
-						</TableBody>
-					</Table>
-				</TableContainer>
-			</div>
-		</div>
-	);
+  if (excelData.length > 0) {
+    return (
+      <div className="flex items-center justify-center h-screen Dashboard">
+        <div className="w-full max-w-3xl p-6 bg-gray-100 rounded-lg">
+          <TableContainer style={{ height: "70vh", overflowY: "auto" }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  {excelData.length > 0 &&
+                    excelData[0].map((header, i) => (
+                      <TableCell key={i}>{header}</TableCell>
+                    ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {excelData.map((row, i) => (
+                  <TableRow key={i}>
+                    {row.map((cell, i) => (
+                      <TableCell key={i}>{cell}</TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      </div>
+    );	
+  } else {
+    return (
+      <div className="flex items-center justify-center h-screen Dashboard">
+        <div className="w-full max-w-3xl p-6 bg-gray-100 rounded-lg">
+          <h1 className="text-2xl font-bold text-center">No data to display</h1>
+        </div>
+      </div>
+    );
+  }
 };
 
 Dashboard.propTypes = {
-	excelData: PropTypes.array.isRequired,
+  excelData: PropTypes.array.isRequired,
 };
 
 export default Dashboard;
--- src/components/buttons/Upload.jsx
+++ src/components/buttons/Upload.jsx
@@ -1,52 +1,63 @@
 import { useDropzone } from "react-dropzone";
+import { useEffect } from "react";
 import "./../styling/Upload.css";
 import { useState } from "react";
 import PropTypes from "prop-types";
 import { read, utils } from "xlsx";
 
 const FileUploader = ({ onSelectFile }) => {
-	const [fileSelected, setFileSelected] = useState(false);
-	// const [excelData, setExcelData] = useState([]);
+  const [fileSelected, setFileSelected] = useState(false);
+  useEffect(() => {
+    // Check local storage for saved file data
+    const savedData = localStorage.getItem("uploadedFileData");
+    if (savedData) {
+      // If data exists in local storage, use it
+      const parsedData = JSON.parse(savedData);
+      onSelectFile({ fileSelected: true, excelData: parsedData });
+      setFileSelected(true);
+    }
+  }, [onSelectFile]);
 
-	const onDrop = (acceptedFiles) => {
-		const file = acceptedFiles[0];
-		const reader = new FileReader();
+  const onDrop = (acceptedFiles) => {
+    const file = acceptedFiles[0];
+    const reader = new FileReader();
 
-		reader.onload = (e) => {
-			const data = new Uint8Array(e.target.result);
-			const workbook = read(data, { type: "array" });
-			const sheetName = workbook.SheetNames[0];
-			const worksheet = workbook.Sheets[sheetName];
-			const parsedData = utils.sheet_to_json(worksheet, { header: 1 });
+    reader.onload = (e) => {
+      const data = new Uint8Array(e.target.result);
+      const workbook = read(data, { type: "array" });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const parsedData = utils.sheet_to_json(worksheet, { header: 1 });
 
-			// setExcelData(parsedData);
-			setFileSelected(true);
-			console.log("File selected in upload:", fileSelected);
-			console.log("Excel data:", parsedData);
-			onSelectFile({fileSelected: true, excelData: parsedData });
-		};
+      localStorage.setItem("uploadedFileData", JSON.stringify(parsedData));
 
-		reader.readAsArrayBuffer(file);
-	};
+      setFileSelected(true);
+      console.log("File selected in upload:", fileSelected);
+      console.log("Excel data:", parsedData);
+      onSelectFile({ fileSelected: true, excelData: parsedData });
+    };
 
-	const { getRootProps, getInputProps } = useDropzone({
-		onDrop,
-		accept:
-			".csv, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-	});
+    reader.readAsArrayBuffer(file);
+  };
 
-	return (
-		<div className="file-upload-container">
-			<div {...getRootProps()} className="dropzone">
-				<input {...getInputProps()} />
-				<p>Drag a drop a CSV or Excel file here, or click to select files</p>
-			</div>
-		</div>
-	);
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept:
+      ".csv, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  });
+
+  return (
+    <div className="file-upload-container">
+      <div {...getRootProps()} className="dropzone">
+        <input {...getInputProps()} />
+        <p>Drag a drop a CSV or Excel file here, or click to select files</p>
+      </div>
+    </div>
+  );
 };
 
 FileUploader.propTypes = {
-	onSelectFile: PropTypes.func.isRequired,
+  onSelectFile: PropTypes.func.isRequired,
 };
 
 export default FileUploader;
