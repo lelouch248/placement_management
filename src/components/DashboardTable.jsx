f8cef089a25e46d226c550911dde0332c84f8d3e
lelouch248 <karthikgkalai@gmail.com>
2023-11-09 22:06:57 +0530

    added modal functionality

--- src/components/DashboardTable.jsx
+++ src/components/DashboardTable.jsx
@@ -9,12 +9,14 @@ import {
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useState } from "react";
 import PropTypes from "prop-types";
+import DeleteModal from "./DeleteModal";
 import "./styling/dashboard.css";
 
 const DashboardTable = ({ excelData }) => {
   // Delete logic
   const [isDeleteButtonShown, setDeleteButtonShown] = useState(false);
   const [columnToDelete, setColumnToDelete] = useState(-1);
+  const [showModal, setShowModal] = useState(false);
 
   const handleMouseEnter = (columnNumber) => {
     setDeleteButtonShown(true);
@@ -29,44 +31,57 @@ const DashboardTable = ({ excelData }) => {
   const handleDeleteColumn = (index) => {
     // Here you can add your MongoDB delete logic
     console.log("Delete column:", index);
+    setShowModal(false);
   };
 
   return (
-    <TableContainer
-      style={{ height: "70vh", overflowY: "auto" }}
-      className="tableContainer">
-      <Table>
-        <TableHead>
-          <TableRow>
-            {excelData.length > 0 &&
-              excelData[0].map((header, i) => (
-                <TableCell
-                  key={i}
-                  className="table-cell"
-                  onMouseEnter={handleMouseEnter.bind(this, i)}
-                  onMouseLeave={handleMouseLeave}>
-                  {isDeleteButtonShown && columnToDelete === i ? (
-                    <DeleteIcon  
-                    className="delete-icon"
-                    onClick={() => handleDeleteColumn(i)} />
-                  ) : (
-                    header
-                  )}
-                </TableCell>
-              ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {excelData.slice(1).map((row, rowIndex) => (
-            <TableRow key={rowIndex}>
-              {row.map((cell, cellIndex) => (
-                <TableCell key={cellIndex}>{cell}</TableCell>
-              ))}
+    <>
+      <TableContainer
+        style={{ height: "70vh", overflowY: "auto" }}
+        className="tableContainer">
+        <Table>
+          <TableHead>
+            <TableRow>
+              {excelData.length > 0 &&
+                excelData[0].map((header, i) => (
+                  <TableCell
+                    key={i}
+                    className="table-cell"
+                    onMouseEnter={handleMouseEnter.bind(this, i)}
+                    onMouseLeave={handleMouseLeave}>
+                    {isDeleteButtonShown && columnToDelete === i ? (
+                      <DeleteIcon
+                        className="delete-icon"
+                        onClick={() => setShowModal(true)}
+                      />
+                    ) : (
+                      header
+                    )}
+                  </TableCell>
+                ))}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {excelData.slice(1).map((row, rowIndex) => (
+              <TableRow key={rowIndex}>
+                {row.map((cell, cellIndex) => (
+                  <TableCell key={cellIndex}>{cell}</TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {showModal && (
+        <DeleteModal
+          showModal={showModal}
+          handleClose={() => setShowModal(false)}
+          handleDelete={() =>
+            handleDeleteColumn(columnToDelete, columnToDelete)
+          }
+        />
+      )}
+    </>
   );
 };
 
