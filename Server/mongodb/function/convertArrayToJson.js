8e38ee0b6f9db2adc49e6a378855defe27b626ef
lelouch248 <karthikgkalai@gmail.com>
2023-11-13 11:08:43 +0530

    added filter to remove empty objects

--- Server/mongodb/function/convertArrayToJson.js
+++ Server/mongodb/function/convertArrayToJson.js
@@ -1,5 +1,10 @@
 const convertArrayToJson = (dataArray, attributes) => {
-  const studentsArray = dataArray.map((studentArray) => {
+  console.log("before filtering" + dataArray.length);
+  var filteredArray = dataArray.filter(
+    (value) => Object.keys(value).length !== 0
+  );
+  console.log("after filtering " + filteredArray.length);
+  const studentsArray = filteredArray.map((studentArray) => {
     // Convert the inner array to an object with named attributes
     const studentObject = Object.fromEntries(
       studentArray.map((value, index) => [attributes[index], value])
